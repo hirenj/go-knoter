def61357034645fc6b1a41ff69dbac7760b0f241
Hiren Joshi <hirenj@gmail.com>
2026-03-24 23:57:18 +0100

    Add formula

---
+++ Formula/knoter.rb
@@ -0,0 +1,38 @@
+class Knoter < Formula
+  desc "Upload HTML reports to Microsoft OneNote"
+  homepage "https://github.com/CopenhagenCenterForGlycomics/knoter-go"
+  license "MIT"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/CopenhagenCenterForGlycomics/knoter-go/releases/download/v#{version}/knoter-darwin-arm64.tar.gz"
+      sha256 "PLACEHOLDER_darwin_arm64"
+    end
+    on_intel do
+      url "https://github.com/CopenhagenCenterForGlycomics/knoter-go/releases/download/v#{version}/knoter-darwin-amd64.tar.gz"
+      sha256 "PLACEHOLDER_darwin_amd64"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/CopenhagenCenterForGlycomics/knoter-go/releases/download/v#{version}/knoter-linux-arm64.tar.gz"
+      sha256 "PLACEHOLDER_linux_arm64"
+    end
+    on_intel do
+      url "https://github.com/CopenhagenCenterForGlycomics/knoter-go/releases/download/v#{version}/knoter-linux-amd64.tar.gz"
+      sha256 "PLACEHOLDER_linux_amd64"
+    end
+  end
+
+  def install
+    bin.install "knoter"
+    bin.install "knoter-auth"
+  end
+
+  test do
+    assert_match "upload HTML", shell_output("#{bin}/knoter help")
+    assert_match "knoter-auth", shell_output("#{bin}/knoter-auth --help 2>&1; true")
+  end
+end
